21bcd5322dca9ae3709ec355046d43482e652e76
thallys97 <thallysbandeira97@gmail.com>
2024-03-28 12:13:49 -0500

    criei a função fetchUserRepos, responsável por buscar os repositórios públicos de um usuário do GitHub.

---
+++ src/services/GitHubService.js
@@ -0,0 +1,14 @@
+export const fetchUserRepos = async (username) => {
+  const endpoint = `https://api.github.com/users/${username}/repos?type=public`;
+  try {
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw new Error('GitHub user not found.');
+    }
+    const repos = await response.json();
+    return repos;
+  } catch (error) {
+    console.error("There was an error fetching the user's repositories:", error);
+    throw error;
+  }
+};
