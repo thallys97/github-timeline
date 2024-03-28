191e4130025341937c24f8eca62f2fbd3f6db704
thallys97 <thallysbandeira97@gmail.com>
2024-03-28 12:18:08 -0500

    alterei o arquivo que renderiza a interface do usuário para adicionar a lógica de buscar os repositórios públicos a partir do nome do usuário

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import UserInputComponent from './components/UserInputComponent';
+import TimelineComponent from './components/TimelineComponent';
+import { fetchUserRepos } from './services/GitHubService';
 
 function App() {
+  const [repos, setRepos] = useState([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleUsernameSubmit = async (submittedUsername) => {
+    try {
+      setIsLoading(true);
+      setError('');
+      const userRepos = await fetchUserRepos(submittedUsername);
+      setRepos(userRepos);
+      setIsLoading(false);
+      // Aqui você irá processar e passar os dados para o componente de timeline
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GitHub Timeline</h1>
+      <UserInputComponent onUsernameSubmit={handleUsernameSubmit} />
+      {isLoading && <p>Loading...</p>}
+      {error && <p className="error">{error}</p>}
+      <TimelineComponent repos={repos} /> {/* Inclua o componente Timeline aqui */}
     </div>
   );
 }
