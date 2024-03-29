f68c61614f2d10be6f51caf9316ed68115ec8114
thallys97 <thallysbandeira97@gmail.com>
2024-03-29 17:12:32 -0500

    criei um método para buscar todos os repositórios dos usuários.

--- src/services/GitHubService.js
+++ src/services/GitHubService.js
@@ -12,3 +12,23 @@ export const fetchUserRepos = async (username, page = 1) => {
     throw error;
   }
 };
+
+export const fetchAllUserRepos = async (username) => {
+  let page = 1;
+  let allRepos = [];
+  let hasMore = true;
+
+  while (hasMore) {
+    const endpoint = `https://api.github.com/users/${username}/repos?type=public&sort=created&per_page=100&page=${page}`;
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw new Error('GitHub user not found.');
+    }
+    const repos = await response.json();
+    allRepos = allRepos.concat(repos);
+    hasMore = repos.length === 100;
+    page++;
+  }
+
+  return allRepos;
+};
